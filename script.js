2b3538b221be1abe5722ebca5ed1f500345ab384
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-07-25 22:08:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ function insert_Row() {
 	
 	let tr=document.createElement('tr');
 	let td1=document.createElement('td');
-	td1.textContent=`Row${row} cell1`;
+	//td1.textContent=`Row${row} cell1`;
 	
 	let td2=document.createElement('td');
-	td2.textContent=`Row${row} cell2`;
+	//td2.textContent=`Row${row} cell2`;
+	td1.textContent='New Cell1';
+	td2.textContent='New Cell2';
 	
 	tr.append(td1,td2);
      table.appendChild(tr);
