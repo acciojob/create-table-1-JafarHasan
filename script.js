1f33bda5a78d91f570ffc11dd4638ba2f57cb014
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-07-25 22:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 const table=document.getElementById('sampleTable');
-let row=3;
 
 function insert_Row() {
-    //Write your code here
+    let tr = document.createElement('tr');
+    let td1 = document.createElement('td');
+    td1.textContent = 'New Cell1';
 	
-	let tr=document.createElement('tr');
-	let td1=document.createElement('td');
-	//td1.textContent=`Row${row} cell1`;
+    let td2 = document.createElement('td');
+    td2.textContent = 'New Cell2';
 	
-	let td2=document.createElement('td');
-	//td2.textContent=`Row${row} cell2`;
-	td1.textContent='New Cell1';
-	td2.textContent='New Cell2';
-	
-	tr.append(td1,td2);
-     table.appendChild(tr);
-	row++;
+    tr.append(td1, td2);
 
-  
-  
+    // Get the first row of the table
+    let firstRow = table.getElementsByTagName('tr')[0];
+
+    // Insert the new row before the first row
+    table.insertBefore(tr, firstRow);
 }
