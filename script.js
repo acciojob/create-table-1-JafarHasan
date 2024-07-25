b18032206a2e29c1eade5ba4cfe2f4ed46b250d0
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-07-25 15:29:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+const table=document.getElementById('sampleTable');
+let row=3;
+let col=3;
 function insert_Row() {
     //Write your code here
+	
+	int tr=document.createElement('tr');
+	int td=document.createElement('td');
+	td.textContent=`Row${row} cell${col}`;
+	
+	tr.appendChild(td);
+     table.appendChild(tr);
+	row++;
+	col++;
   
   
 }
